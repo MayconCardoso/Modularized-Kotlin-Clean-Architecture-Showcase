376a859996efb465c25fd6cf21412be747d64323
mayconcardoso <maycon.santos.cardoso@gmail.com>
2019-11-19 23:55:25 -0300

    * Random quotation feature

--- libraries/library-shared-feature-arq-testing/src/main/java/com/mctech/test/arq/BaseViewModelTest.kt
+++ libraries/library-shared-feature-arq-testing/src/main/java/com/mctech/test/arq/BaseViewModelTest.kt
@@ -2,12 +2,14 @@ package com.mctech.test.arq
 
 import com.mctech.test.arq.rules.CoroutinesMainTestRule
 import com.mctech.test.arq.rules.KoinModuleTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
 import org.koin.test.AutoCloseKoinTest
 
 /**
  * @author MAYCON CARDOSO on 2019-09-25.
  */
+@ExperimentalCoroutinesApi
 abstract class BaseViewModelTest : AutoCloseKoinTest() {
     @get:Rule
     val koinRule = KoinModuleTestRule()
